32d551a1e47c608925998b091b3a2cb5d8720f83
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-04-18 21:40:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var romlist = document.getElementById("HMR3_dropdown");
+        //Create array of options to be added
+        var HMR3_optionText = [
+            "Please Choose", 
+            "Pokemon Clover", 
+            "Pokemon Flora Sky", 
+            "Pokemon Leaf Green", 
+            "Pokemon Moon Galaxy"
+        ];
 
-var newOption = document.createElement('option');
-newOption.innerText = "New value";
-newOption.setAttribute('value', 'newvalue');
-
-romlist.appendChild(newOption);
+        var HMR3_optionValue = [
+            "#", 
+            "https://rawcdn.githack.com/moondoges/cyanide/a02ac068975c5383c3d297c531a85eff04fbcfdf/pokemon_clover.gba", //link not working
+            "https://rawcdn.githack.com/srojam/gba/58893088e3805baaf716e0af6801aaeeae1a0efb/Binaries/pokemonflorasky.gba", 
+            "https://rawcdn.githack.com/srojam/gba/58893088e3805baaf716e0af6801aaeeae1a0efb/Binaries/pokemongreen.gba", 
+            "https://rawcdn.githack.com/vbaemulator/GBA-Roms/f8ff9ba91598a5e996f29c7c27342f062faa989d/Pokemon%20Moon%20Galaxy.zip"
+        ];
