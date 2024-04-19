6f6bf01ff25aadcbfbb2219ab8501e17cbbd331d
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-04-19 08:10:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 //Create array of options to be added
 var HMR3_optionText = [
     "Please Choose", 
-    "Pokemon Clover", 
+    "Pokemon Clover (currently not working)", 
     "Pokemon Flora Sky", 
     "Pokemon Leaf Green", 
     "Pokemon Moon Galaxy",
     "Dark Worship",
     "Emerald Horizions",
-    "Exceeded"
+    "Exceeded",
+    "LGPika", //start here with links
+    "Mirage of Tales",
+    "PK Theta",
+    "Pokemon Elysium",
+    "Pokemon Mercury Azoth",
+    "Pokemon Ultra Gold Sigma",
+    "Pokemon Yuval",
+    "Re:Verse",
+    "ROWE",
+    "Radical Red",
+    "Ruby Renev",
+    "Shiron Castle",
+    "Unknown",
+    "Crest",
+    "Nameless",
+    "Regis' Origin"
+    
+    
 ];
 
 var HMR3_optionValue = [
@@ -18,5 +36,22 @@ var HMR3_optionValue = [
     "https://rawcdn.githack.com/vbaemulator/GBA-Roms/f8ff9ba91598a5e996f29c7c27342f062faa989d/Pokemon%20Moon%20Galaxy.zip",
     "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Dark%20Worship%20ENGLISH%20COMPLETE.Gba",
     "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/EmeraldHorizonsv1.09.gba",
-    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Exceeded%20v11.5.gba"
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Exceeded%20v11.5.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/LGPika%20(v8.0).gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Mirage%20of%20Tales%20(Alpha%20v3.3).gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Odyssey%20Beta%20v2.6.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/PK%20Theta%20EM%20721%20Ex%2027-02-17.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Pokemon%20Elysium_A.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Pokemon%20Mercury%20Azoth%20Project%20(Demo%20v0.011).gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Pokemon%20Ultra%20Shiny%20Gold%20Sigma%20Completo%201.5.0.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/PokemonYuvalVersion.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/RE%20Verse%20v0.9.1.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/ROWE%20(v1.9.4).gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Radical%20Red.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/RubyRenev%20v1.4.2.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Shiron%20Castle%20(v2.0).gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/Unknown%20(v1.0).gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/crest%20v1.0.9.6.3.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/nameless%20v5.30.gba",
+    "https://raw.githack.com/TrapjawwS/funnyemulatorjs/main/romhacks/regis'origin.gba"
 ];
