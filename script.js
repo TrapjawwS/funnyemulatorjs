47c7a8840df2742e4d913069bd13772e3cf0cb6b
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-04-25 22:32:25 +0000

    fixed password

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ if (!link) {
 link.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico';
 
 password = prompt("What's the password?")
-if password == "ligma" {
+if (password == "ligma") {
     alert("good job behaving yourself")
     alert("Note, remove this on the 1st")
-{ else {        
+} else {        
     alert("haha no games because michael was misbehaving suck it bozos maybe you should do your homework for once")
     window.location.replace("https://www.desmos.com/calculator");
 }
