f741800dca5cadb3bd89cee0b5c3ef04239d8406
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-04-25 15:24:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ if (!link) {
 }
 link.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico';
 
-alert("haha no games because michael was misbehaving suck it bozos maybe you should do your homework for once")
-window.location.replace("https://www.desmos.com/calculator");
+password = prompt("What's the password?")
+if password == "ligma" {
+    alert("good job behaving yourself")
+{ else {        
+    alert("haha no games because michael was misbehaving suck it bozos maybe you should do your homework for once")
+    window.location.replace("https://www.desmos.com/calculator");
+}
 
 //Create array of options to be added
 var HMR3_optionText = [
