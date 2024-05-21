72fe7433b3adedfcd784c00730a00608282c4caf
thisismyaltaccountt <156016767+thisismyaltaccountt@users.noreply.github.com>
2024-05-21 18:37:36 +0000

    yes

--- script.js
+++ script.js
@@ -8,14 +8,6 @@ if (!link) {
 }
 link.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico';
 
-var serverversion = 1
-
-if (cleintversion == undefined) {
-    alert("Your client is outdated. Please download the new one at https://github.com/TrapjawwS/funnyemulatorjs/tree/main")
-} else if (serverversion>cleintversion) {
-    alert("Your client is outdated. Please download the new one at https://github.com/TrapjawwS/funnyemulatorjs/tree/main")
-}
-
 var HMR3_optionText = [
     "Please Choose", 
     "Custom URL",
