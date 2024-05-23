dc3606ec60217b815133ef30379a090a1dce507d
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-05-23 16:02:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (!link) {
     link.rel = 'icon';
     document.head.appendChild(link);
 }
-link.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico';
+link.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon-legacy.ico';
 
 var HMR3_optionText = [
     "Please Choose", 
